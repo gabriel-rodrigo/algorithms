9768e51ef0973136e588651fcdd52a6f5405c0f9
Gabriel Rodrigo <gabrielrodrigodcs@gmail.com>
2019-08-08 10:55:58 -0300

    Remove for desnecessário

--- CeasarCipher/CeasarCipher.py
+++ CeasarCipher/CeasarCipher.py
@@ -2,14 +2,13 @@ def ciphertext(text=''):
     # Guard translated letters
     new_word = []
 
-    for word in text:
-        for letter in word:
-            # If letter is space append space
-            # Else append letter ciphered letter
-            if ord(letter) == 32:
-                new_word.append(' ')
-            else:
-                new_word.append(chr(ord(letter)+3))
+    for letter in text:
+        # If letter is space append space
+        # Else append letter ciphered letter
+        if ord(letter) == 32:
+            new_word.append(' ')
+        else:
+            new_word.append(chr(ord(letter)+3))
     
     # Join ciphered letters to return as string
     print('in : ' + text)
@@ -19,14 +18,13 @@ def deciphertext(text=''):
     # Guard translated letters
     new_word = []
 
-    for word in text:
-        for letter in word:
-            # If letter is space append space
-            # Else append letter deciphered letter
-            if ord(letter) == 32:
-                new_word.append(' ')
-            else:
-                new_word.append(chr(ord(letter)-3))
+    for letter in text:
+        # If letter is space append space
+        # Else append letter deciphered letter
+        if ord(letter) == 32:
+            new_word.append(' ')
+        else:
+            new_word.append(chr(ord(letter)-3))
     
     # Join deciphered letters to return as string
     print('in : ' + text)
@@ -34,6 +32,10 @@ def deciphertext(text=''):
 
 
 
-ciphertext('gabriel rodrigo')
+ciphertext('gabriel rodrigo da costa silva')
+print()
+ciphertext('Gabriel Rodrigo da Costa Silva')
+print()
+ciphertext('GABRIEL RODRIGO DA COSTA SILVA')
 print()
 deciphertext('jdeulho urguljr')
